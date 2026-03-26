a761d8ea58e1097b317b817eff394c185b872b83
Yousef Dawood <yousefdawood31@gmail.com>
2026-03-27 01:04:22 +0200

    update .gitignore and implement product card rendering in overview view

--- index.ts
+++ index.ts
@@ -1,6 +1,50 @@
+import { readFileSync } from "fs";
 import http from "http";
+import path from "path";
 
-/*
+const data = JSON.parse(
+  readFileSync(path.join(import.meta.dirname, "data", "data.json"), "utf-8"),
+);
+
+const overviewPage = readFileSync(
+  path.join(import.meta.dirname, "views", "overview", "overview.html"),
+  "utf-8",
+);
+
+const overviewCSS = readFileSync(
+  path.join(import.meta.dirname, "views", "overview", "styles.css"),
+  "utf-8",
+);
+
+const productPage = readFileSync(
+  path.join(import.meta.dirname, "views", "product", "product.html"),
+  "utf-8",
+);
+
+const productCSS = readFileSync(
+  path.join(import.meta.dirname, "views", "product", "product.html"),
+  "utf-8",
+);
+
+const card = readFileSync(
+  path.join(import.meta.dirname, "views", "product", "product_card.html"),
+  "utf-8",
+);
+
+type ProductType = {
+  id: string;
+  productName: string;
+  image: string;
+  from: string;
+  nutrients: string;
+  quantity: string;
+  price: string;
+  organic: boolean;
+  description: string;
+};
+
+const replaceTemplate = function (template: string, product: ProductType) {
+  /*
     {%PRODUCT_NAME%}
     {%PRODUCT_EMOJI%}
     {%IS_PRODUCT_ORGANIC%}
@@ -10,9 +54,35 @@ import http from "http";
     {%PRODUCT_PRICE%}
     {%PRODUCT_DESC%}
     {%PRODUCT_URL%}
+    {%PRODUCT_CARD%}
 */
 
-const server = http.createServer((req, res) => {});
+  return template
+    .replaceAll("{%PRODUCT_NAME%}", product.productName)
+    .replaceAll("{%PRODUCT_EMOJI%}", product.image)
+    .replaceAll("{%IS_PRODUCT_ORGANIC%}", product.organic ? "" : "not-organic")
+    .replaceAll("{%PRODUCT_FROM%}", product.from)
+    .replaceAll("{%PRODUCT_NUTRIENTS%}", product.nutrients)
+    .replaceAll("{%PRODUCT_QUANTITY%}", product.quantity)
+    .replaceAll("{%PRODUCT_PRICE%}", product.price)
+    .replaceAll("{%PRODUCT_DESC%}", product.description)
+    .replaceAll(
+      "{%PRODUCT_URL%}",
+      `${process.env.APP_URL ?? "localhost:3000"}/product/${product.id}`,
+    );
+};
+
+const replaceCard = function (template: string, products: ProductType[]) {
+  return template.replaceAll(
+    "{%PRODUCT_CARD%}",
+    products.map((product) => replaceTemplate(card, product)).join("\n"),
+  );
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && ["/", "/overview"].includes(req.url!)) {
+  }
+});
 
 server.listen(3000, () => {
   console.log("Server listening on port 3000");
